9e016b8c6fad5a3539a424cae1e83222a34e3ad3
Derek Damron <damrondm@gmail.com>
2015-11-01 15:36:24 -0800

    Update trim functions

--- R/trim_percent.R
+++ R/trim_percent.R
@@ -19,7 +19,6 @@ trim_percent <- function(x, lo=0, hi=1) {
   }
   lo_val <- quantile(x, prob=lo, type=8)
   hi_val <- quantile(x, prob=hi, type=8)
-  x[x <= lo_val] <- lo_val
-  x[x >= hi_val] <- hi_val
+  x <- trim_value(x, lo=lo_val, hi=hi_val)
   return(x)
 }
