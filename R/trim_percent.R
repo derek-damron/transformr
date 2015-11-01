250cd23ccd1d0dfd082f010d70112d3f3bd7e3e9
Derek Damron <damrondm@gmail.com>
2015-11-01 15:34:00 -0800

    Add trim_value

--- R/trim_percent.R
+++ R/trim_percent.R
@@ -1,3 +1,18 @@
+trim_value <- function(x, lo, hi) {
+  if (missing(x)) {
+    stop('Please provide a vector x')
+  }
+  if (missing(lo)) {
+    stop('Please provide a lower value lo')
+  }
+  if (missing(hi)) {
+    stop('Please provide a lower value lo')
+  }
+  x[x <= lo] <- lo
+  x[x >= hi] <- hi
+  return(x)
+}
+
 trim_percent <- function(x, lo=0, hi=1) {
   if (missing(x)) {
     stop('Please provide a vector x')
