2723f869905b689a189a068a78dace90a808fa39
Derek Damron <damrondm@gmail.com>
2015-11-01 18:24:05 -0800

    Create generic trim function

---
+++ R/trim.R
@@ -0,0 +1,25 @@
+trim <- function(x, lo, hi, type=c('value','percentile')) {
+  if (missing(x)) {
+    stop('Please provide a vector x')
+  }
+  if (missing(lo)) {
+    stop('Please provide a lower value lo')
+  }
+  if (missing(hi)) {
+    stop('Please provide a upper value hi')
+  }
+  if (missing(type)) {
+    warning('No type provided - Using value')
+    type <- 'value'
+  }
+  type <- match.arg(type)
+  if (type == 'percentile') {
+    lo <- lo / 100
+    hi <- 1 - hi / 100
+    lo <- quantile(x, prob=lo, type=8)
+    hi <- quantile(x, prob=hi, type=8)
+  }
+  x[x <= lo] <- lo
+  x[x >= hi] <- hi
+  return(x)
+}
