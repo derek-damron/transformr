7d18404982a387e494020f31c053a9706391c071
Derek Damron <damrondm@gmail.com>
2016-03-11 21:49:09 -0800

    Add impute_sample function

---
+++ R/impute_sample.R
@@ -0,0 +1,43 @@
+#' Helper function that samples with replacement
+#'
+#' \code{impute} returns a vector of values imputed from resampling \code{x}.
+#'
+#' @param x A vector.
+#' @param n The number of values to impute.
+#' @param replace Whether or not to sample with replacement.  The default is TRUE.
+#' @return The output of \code{impute_sample} is a vector of imputed values of length \code{n}
+#' @export
+#' @examples
+#' x <- c(NA, "mode", "mode", "other", NA)
+#'
+#' # Impute via sampling
+#' x_impute <- impute(x, impute_sample)
+#'
+#' # Compare
+#' data.frame(x, x_impute)
+impute_sample <- function(x, n=1, replace=TRUE) {
+    # Check x
+    if (missing(x)) {
+        stop("Please provide a vector x to use for sampling", call.=FALSE)
+    }
+
+    # Tabulate
+    x_tab <- table_rcpp(x)
+
+    # Convert counts to percentages
+    x_prob <- x_tab / length(x)
+
+    # Unique values
+    x_unique <- names(x_tab)
+
+    # Sample
+    x_sample <- sample(x_unique, size=n, replace=replace, prob=x_prob)
+
+    # Convert back to numeric, if applicable
+    if (is.numeric(x)) {
+        x_sample <- as.numeric(x_sample)
+    }
+
+    # Return
+    x_sample
+}
---
+++ tests/testthat/test_impute_5_sample.R
@@ -0,0 +1,46 @@
+context('Impute - impute_sample')
+
+test_that('args', {
+  expect_error(impute_sample(),
+               'Please provide a vector x to use for sampling')
+})
+
+#####
+# Numeric
+#
+
+x <- c(NA, 1, 1, 1, NA, 1, 2, 3, NA)
+
+test_that('numeric - no ties', {
+    set.seed(666)
+    expect_equal(impute(x, impute_sample),
+                 c(3, 1, 1, 1, 1, 1, 2, 3, 2))
+})
+
+x <- c(NA, 1, 1, 1, NA, 2, 2, 2, NA)
+
+test_that('numeric - ties', {
+    set.seed(666)
+    expect_equal(impute(x, impute_sample),
+                 c(1, 1, 1, 1, 2, 2, 2, 2, 1))
+})
+
+#####
+# Character
+#
+
+x <- c(NA, "a", "a", "a", NA, "a", "b", "c", NA)
+
+test_that('no ties', {
+    set.seed(666)
+    expect_equal(impute(x, impute_sample),
+                 c("c", "a", "a", "a", "a", "a", "b", "c", "b"))
+})
+
+x <- c(NA, "a", "a", "a", NA, "b", "b", "b", NA)
+
+test_that('ties', {
+    set.seed(666)
+    expect_equal(impute(x, impute_sample),
+                 c("a", "a", "a", "a", "b", "b", "b", "b", "a"))
+})
